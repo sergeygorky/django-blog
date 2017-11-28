93a2db8d5a9272c03f3687949a363f5f593aad5b
Sergey Gorky <sergeygorky@yahoo.com>
2017-11-28 19:59:21 +0200

    add content brief to admin

--- posts/admin.py
+++ posts/admin.py
@@ -5,14 +5,14 @@ from .models import Post
 
 
 class PostModelAdmin(admin.ModelAdmin):
-    list_display = ['title', 'updated', 'timestamp']
+    list_display = ['title', 'content_brief', 'updated', 'timestamp']
     list_display_links = ['updated', 'timestamp']
     list_editable = ['title']
     list_filter = ['updated', 'timestamp']
     search_fields = ['title', 'content']
 
-    class Meta:
-        model = Post
+    def content_brief(self, post):
+        return post.content.split('.')[0] + '.'
 
 
 admin.site.register(Post, PostModelAdmin)
