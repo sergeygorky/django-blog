3853acc383cdfb96ed9e2ef0b7e17aff3e53164f
Sergey <sergeygorky@yahoo.com>
2018-01-03 21:49:08 +0200

    update django version

--- posts/admin.py
+++ posts/admin.py
@@ -1,18 +1,3 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Post
-
-
-class PostModelAdmin(admin.ModelAdmin):
-    list_display = ['title', 'content_brief', 'updated', 'timestamp']
-    list_display_links = ['updated', 'timestamp']
-    list_editable = ['title']
-    list_filter = ['updated', 'timestamp']
-    search_fields = ['title', 'content']
-
-    def content_brief(self, post):
-        return post.content.split('.')[0] + '.'
-
-
-admin.site.register(Post, PostModelAdmin)
--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,3 @@
-from django.http import HttpResponse
 from django.shortcuts import render
 
-
-def post_home():
-    return HttpResponse("<h1>Hello!</h1>")
+# Create your views here.
