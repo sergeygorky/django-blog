e2392efbab1d35da0790bb9ecb26908e3302d4c3
Sergey <sergeygorky@yahoo.com>
2018-01-03 22:19:03 +0200

    request, response, add django to gitignore

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Post
 
 # Register your models here.
+from .models import Post
+
+
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ['title', 'content_brief', 'updated', 'timestamp']
+    list_display_links = ['updated', 'timestamp']
+    list_editable = ['title']
+    list_filter = ['updated', 'timestamp']
+    search_fields = ['title', 'content']
+
+    def content_brief(self, post):
+        return post.content.split('.')[0] + '.'
+
+
+admin.site.register(Post, PostModelAdmin)
