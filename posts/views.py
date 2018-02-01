ae3d1e59299b871d6c0c7f680bb1f8505704cc3b
Sergey <sergeygorky@yahoo.com>
2018-02-01 12:02:31 +0200

    template context

--- posts/views.py
+++ posts/views.py
@@ -7,15 +7,24 @@ from django.http import HttpResponse
 def post_create(request):
     return HttpResponse("<h1>Create</h1>")
 
+
 def post_detail(request):
-    return HttpResponse("<h1>Detail</h1>")
+    context = {
+        'title': 'Detail'
+    }
+    return render(request, 'posts/index.html', context)
+
 
 def post_list(request):
-    # return HttpResponse("<h1>List</h1>")
-    return render(request, 'posts/index.html', {})
+    context = {
+        'title': 'List'
+    }
+    return render(request, 'posts/index.html', context)
+
 
 def post_update(request):
     return HttpResponse("<h1>Update</h1>")
+
 
 def post_delete (request):
     return HttpResponse("<h1>Delete</h1>")
