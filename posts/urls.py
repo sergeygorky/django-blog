d2c2cbd29e9264b276e559ffc3627512df831a1e
Sergey <sergeygorky@yahoo.com>
2018-02-01 11:52:52 +0200

    django templates

--- posts/urls.py
+++ posts/urls.py
@@ -2,11 +2,15 @@ from django.contrib import admin
 from posts import views
 from django.urls import include, path, re_path
 from . import views
+from .views import post_create
+from .views import post_detail
+from .views import post_update
+from .views import post_delete
 
 urlpatterns = [
     re_path('^$', views.post_list),
-    re_path('^create/$', views.post_create),
-    re_path('^detail/$', views.post_detail),
-    re_path('^update/$', views.post_update),
-    re_path('^delete/$', views.post_delete),
+    re_path('^create/$', post_create),
+    re_path('^detail/$', post_detail),
+    re_path('^update/$', post_update),
+    re_path('^delete/$', post_delete),
 ]
