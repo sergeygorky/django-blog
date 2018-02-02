93b715282dccbeb5eff385bdbff1eab2307ad518
Sergey <sergeygorky@yahoo.com>
2018-02-02 16:23:17 +0200

    Django Messages Framework

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse, HttpResponseRedirect
-
+from django.contrib import messages
 from .forms import PostForm
 from .models import Post
 
@@ -10,7 +10,10 @@ def post_create(request):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, 'Successfully Created')
         return HttpResponseRedirect(instance.get_absolute_url())
+    else:
+        messages.error(request, 'Not Successfully Created')
     context = {
         'form': form,
 
@@ -42,6 +45,7 @@ def post_update(request, id=None):
     if form.is_valid():
         instance = form.save(commit=False)
         instance.save()
+        messages.success(request, '<a href="#">Item</a>Saved', extra_tags='html_safe')
         return HttpResponseRedirect(instance.get_absolute_url())
     context = {
         'title': instance.title,
