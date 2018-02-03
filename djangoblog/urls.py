fe884be6655851dfce0c52b95097620235197e36
Sergey <sergeygorky@yahoo.com>
2018-02-03 12:33:37 +0200

    setup static files

--- djangoblog/urls.py
+++ djangoblog/urls.py
@@ -16,11 +16,18 @@ Including another URLconf
 from django.contrib import admin
 from posts import views
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from django.apps import apps
+
 posts_name = apps.get_app_config('posts').verbose_name
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include(("posts.urls", posts_name), namespace='posts')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
