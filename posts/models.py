726daa172d96be18957130637e8c82cec6288372
Sergey <sergeygorky@yahoo.com>
2018-02-05 17:57:58 +0200

    NoReverseMatch at /posts/

--- posts/models.py
+++ posts/models.py
@@ -12,8 +12,7 @@ class Post(models.Model):
         return self.title
 
     def get_absolute_url(self):
-        # return reverse("posts:detail", kwargs={"id": self.id})
-        return reverse("posts:detail")
+        return reverse("posts:detail", kwargs={"id": self.id})
 
     class Meta:
         ordering = ['-timestamp', '-updated']
