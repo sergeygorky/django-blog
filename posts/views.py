3a58da4f16881ae1fa2d4b9841bc40dda29cec2f
Sergey <sergeygorky@yahoo.com>
2018-02-06 16:09:48 +0200

    Social Share LInks

--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from urllib.parse import quote
 from django.shortcuts import render, get_object_or_404, redirect
 from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib import messages
@@ -23,9 +24,11 @@ def post_create(request):
 
 def post_detail(request, id=None):
     instance = get_object_or_404(Post, id=id)
+    share_string = quote(instance.content)
     context = {
         'title': instance.title,
         'instance': instance,
+        'share_string': share_string
     }
     return render(request, 'posts/post_detail.html', context)
 
